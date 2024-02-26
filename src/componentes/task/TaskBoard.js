69c1fe98294ef4d1d5b212e929041635e5478657
Cristian Santander <casdesarrollo.2022@gmail.com>
2024-02-26 17:26:20 -0300

    agregando tablero central de tareas

--- src/componentes/task/TaskBoard.js
+++ src/componentes/task/TaskBoard.js
@@ -1,42 +1,49 @@
 import styled from 'styled-components';
 import { useState } from "react";
 
-const DrawerContainer  = styled.div`
-  margin: 0;
-  padding: 0;
-  background-color: #171719;
-  border: solid;
-  border-radius: 10px;
+const DrawerContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 70%;
+  justify-content: space-between; /* Distribuye los elementos con espaciado igual */
+  min-height: 300px; /* Tamaño mínimo del contenedor */
+  min-width: 800px; /* Ancho mínimo del contenedor */
 `;
 
 const Item = styled.div`
+  background-color: #0F0F0F;
   color: white;
   width: 200px;
   height: 100px;
-  margin: 10px;
-  border: 1px solid white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  margin: 5px;
+  border: 2px solid #1A1A1D;
+  border-radius: 10px;
+  position: relative; /* Establece el contexto de posicionamiento para los elementos hijos */
 `;
 
-function TaskBoard () {
+const ItemText = styled.div`
+  font-family: sans-serif;
+  font-size: 13px;
+  color: #6F6F85;
+  text-align: start;
+  margin-top: 5px; /* Ajuste de margen superior */
+  margin-left: 10px; /* Ajuste de margen izquierdo */
+  position: absolute; /* Posiciona de forma absoluta */
+  top: 0; /* Lo posiciona en la esquina superior */
+  left: 0; /* Lo posiciona en la esquina izquierda */
+`;
 
-  const [items, setItems] = useState(["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]);
+function TaskBoard() {
 
-  const handleAddItem = () => {
-    setItems([...items, `Item ${items.length + 1}`]);
-  };
+  const [items, setItems] = useState([]); // Inicialmente vacío
+  const statuses = ["PARA HACER", "EN PROCESO", "FINALIZADO", "EN REVISIÓN", "APROBADO"]; // Textos para cada item
 
   return (
     <DrawerContainer>
-        {items.map((item, index) => (
-          <Item key={index}>{item}</Item>
-        ))}
+      {statuses.map((status, index) => (
+        <Item key={index}>
+          <ItemText>{status}</ItemText>
+          {items[index] ? items[index] : null}
+        </Item>
+      ))}
     </DrawerContainer>
   );
 }
