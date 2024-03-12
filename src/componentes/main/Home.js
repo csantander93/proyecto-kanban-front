c198df1706e4007277c5e245c9ac96a55b126fee
KevinOviedo12 <kevinoviedo2018@gmail.com>
2024-03-12 12:45:31 -0300

    barra de perfil y notificaciones correctas, falta darle funcionanlidad a los botones de cerrar sesion y editar perfil

--- src/componentes/main/Home.js
+++ src/componentes/main/Home.js
@@ -7,6 +7,7 @@ import FormProject from "../project/FormProject";
 import TaskBoard from "../task/TaskBoard";
 import GlobalStyles from "./GlobalStyles";
 import SearchProject from "../project/SearchProject";
+import { IoIosNotifications } from "react-icons/io";
 
 const Container = styled.div`
   margin-left: 170px;
@@ -18,14 +19,6 @@ const Container = styled.div`
   display: flex;
   justify-content: center; /* Centra horizontalmente */
   align-items: center; /* Centra verticalmente */
-`;
-
-const H1 = styled.h1 `
-  font-family: sans-serif;
-  text-align: center;
-  border: none;
-  border-radius: 10px;
-  background-color: #22D9EE;
 `;
 
 const Header = styled.header `
@@ -41,11 +34,90 @@ const Header = styled.header `
   overflow: hidden;
   border-right: 2px solid #272729; /* Color del borde más claro */
 `;
+const Perfil = styled.button `
+
+font-family: sans-serif;
+
+  background-color: #1d90cc;
+  position: absolute;
+  top: 0;
+  right: 0;
+   padding: 10px; /* Añade un poco de espacio interno para que no esté pegado al borde */
+   margin:20px;
+border: none; /* Quita el borde para un aspecto más limpio */
+  cursor: pointer; /* Cambia el cursor al pasar sobre el botón */
+  transition: background-color 0.3s; /* Agrega una transición suave al cambio de color */
+border-radius:10px;
+  &:hover {
+    background-color: #276465;
+  }
+`;
+// Estilizando la ventana desplegable
+const DropdownContent = styled.div`
+  display: ${(props) => (props.isOpen ? 'block' : 'none')};
+  position: absolute;
+  top: 70px;
+  right: 0;
+  background-color: rgba(172, 158, 156, 0.4);
+  min-width: 100px;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  margin-right: 20px;
+  padding: 12px 16px;
+  z-index: 1;
+  border: none;
+  border-radius: 10px;
+
+  a {
+    font-family: sans-serif;
+    display: block;
+    margin-bottom: 15px;
+    text-decoration: none;
+    color: white;
+    position: relative;
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      bottom: -2px;
+      left: 0;
+      background-color: #1d90cc;
+      transform-origin: bottom left;
+      transition: transform 0.25s ease-out;
+      transform: scaleX(0);
+    }
+
+    &#editar:hover::after {
+      transform: scaleX(0.8);
+    }
+     &#cerrar:hover::after {
+      transform: scaleX(1);
+    }
+  }
+`;
+
 
 const TaskBoardContainer = styled.div`
   background-color: #171719; /* Mismo color de fondo que el Header */
   padding: 20px; /* Espaciado interior */
   border-radius: 10px; /* Borde redondeado */
+`;
+
+const Campana = styled(IoIosNotifications )`
+margin-right: 6px;
+font-size: 45px;
+margin-top:18px;
+color:#1d90cc;
+&:hover {
+  font-size: 50px;
+  cursor: pointer;
+  transition: font-size 0.7s; /* Controla la velocidad de cambio en el hover */
+  filter: brightness(70%); /* Reduce el brillo al pasar el cursor */
+}
+position: absolute;
+top: 0;
+right: 60px; /* Ajusta el valor de left según sea necesario */
 `;
 
 function Home () {
@@ -70,18 +142,31 @@ function Home () {
   const actualizarProyectos = () => {
     fetchData();
   }
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
     <GlobalStyles/>
+    
       <Header>
-        <H1>{ user.nombre[0] }{ user.apellido[0] } </H1>
+        
         <FormProject actualizarProyectos={actualizarProyectos} />
         <SearchProject></SearchProject>
         <ProjectList listaProyectos={proyectos} actualizarProyectos={actualizarProyectos} />
       </Header>
       <Container>
-        <TaskBoardContainer>
+        <div>
+          <Campana />
+          <Perfil onClick={toggleDropdown}>{ user.nombre[0] }{ user.apellido[0] } </Perfil>
+          <DropdownContent isOpen={isOpen}>
+                  <a id="cerrar"href="#">Cerrar sesión</a>
+                  <a id="editar"href="#">Editar perfil</a>
+          </DropdownContent>
+        </div>
+        <TaskBoardContainer>s
           <TaskBoard />
         </TaskBoardContainer>
       </Container>
